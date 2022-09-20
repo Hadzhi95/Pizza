538231b4cb6ac0c9fa5b32ac16cb0029ad810ad0
Hadzhi Yakhyaev <kreyg1992@bk.ru>
2022-09-20 23:10:09 +0300

    Router

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import Categories from './components/Categories';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 import './scss/app.scss'
-import pizzas from './assets/pizzas.json'
 
 
 function App() {
+  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj}
-              // title={obj.title} 
-              // price={obj.price} 
-              // imageUrl={obj.imageUrl} 
-              // sizes={obj.sizes}
-              // types={obj.types}
-              />)
-            }
-          </div>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+          {/* <Home/> */}
+          {/* <NotFound/> */}
         </div>
       </div>
     </div>
---
+++ react-pizza/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import { Skeleton } from '../components/PizzaBlock/Skeleton';
+import Sort from '../components/Sort';
+
+function Home() {
+    const [items, setItems] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        fetch("https://6329d2b14c626ff832cb763a.mockapi.io/items")
+            .then((res) => res.json())
+            .then((arr) => setItems(arr))
+        setIsLoading(false)
+    }, [])
+    return (
+        <>
+            <div className="content__top">
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {
+                    isLoading
+                        ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                        : items.map((obj) => <PizzaBlock key={obj.id} {...obj}
+                        // items.map((obj) => isLoading? <Skeleton/> :<PizzaBlock key={obj.id} {...obj}
+                        // title={obj.title} 
+                        // price={obj.price} 
+                        // imageUrl={obj.imageUrl} 
+                        // sizes={obj.sizes}
+                        // types={obj.types}
+                        />)
+                }
+            </div>
+        </>
+    )
+}
+
+export default Home
